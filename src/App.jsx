c801acb0200be1ebf90ae4db456cb5a8967a92cf
shencanet <santi.dionis@gmail.com>
2024-06-12 19:42:00 +0200

    ADD: FORMULARIO

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,10 @@
-const   App = () => { return 'APP'}
+const   App = () => {
+    return (
+    <div className="container">
+        <h1>Formulario</h1>
+        
+    </div>
+    )
+
+}
 export default App;
--- src/Components/NoControlado.jsx
+++ src/Components/NoControlado.jsx
@@ -1,5 +1,35 @@
 const NoControlado = () => {
-    return(' No controlado');}
+    return(
+
+        <form>
+            <input type="text" 
+             id="nombre" 
+             placeholder="introduce el texto"
+             className="form-control mb-2"/>
+
+             <textarea className="form-control mb-2" 
+             placeholder="Introduce descripcion"/>
+             <select>
+                <option value="1">Enero</option>
+                <option value="2">Febrero</option>
+                <option value="3">Marzo</option>
+                <option value="4">Abril</option>
+                <option value="5">Mayo</option>
+                <option value="6">Junio</option>
+                <option value="7">Julio</option>
+                <option value="8">Agosto</option>
+                <option value="9">Septiembre</option>
+                <option value="10">Octubre</option>
+                <option value="11">Noviembre</option>
+                <option value="12">Diciembre</option>
+                
+             </select>
+
+        
+
+        
+        </form>
+    );}
 
 
     export default NoControlado;
