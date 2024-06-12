ddf2057aa8f6819a059965b09f69ff962f8b836d
shencanet <santi.dionis@gmail.com>
2024-06-12 20:32:47 +0200

    ADD: FORMULARIO

--- src/Components/NoControlado.jsx
+++ src/Components/NoControlado.jsx
@@ -1,42 +1,51 @@
-const NoControlado = () => {
-    return(
+import { useRef } from "react";
 
-        <div className="container mt-2">
-        <form
-          onSubmit={handleSubmit}
-          ref={formulario}
+const FormularioNoControlado = () => {
+  const formulario = useRef(null);
+
+  // Formulario no controlado
+  const handleSubmit = (e) => {
+    console.log(formulario.current);
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container mt-2">
+      <form
+        onSubmit={handleSubmit}
+        ref={formulario}
+      >
+        <input
+          className="form-control mb-2"
+          type="text"
+          placeholder="Ingrese un TODO"
+          name="todoNombre"
+          defaultValue="Tarea #01"
+        />
+        <textarea
+          className="form-control mb-2"
+          type="text"
+          placeholder="Ingrese un TODO"
+          name="todoDescripcion"
+          defaultValue="Descripción tarea #01"
+        />
+        <select
+          className="form-control mb-2"
+          name="todoEstado"
+          defaultValue="completado"
         >
-          <input
-            className="form-control mb-2"
-            type="text"
-            placeholder="Ingrese un TODO"
-            name="todoNombre"
-            defaultValue="Tarea #01"
-          />
-          <textarea
-            className="form-control mb-2"
-            type="text"
-            placeholder="Ingrese un TODO"
-            name="todoDescripcion"
-            defaultValue="Descripción tarea #01"
-          />
-          <select
-            className="form-control mb-2"
-            name="todoEstado"
-            defaultValue="completado"
-          >
-            <option value="pendiente">Pendiente</option>
-            <option value="completado">Completado</option>
-          </select>
-          <button
-            className="btn btn-primary"
-            type="submit"
-          >
-            Agregar
-          </button>
-        </form>
-      </div>
-    );}
+          <option value="pendiente">Pendiente</option>
+          <option value="completado">Completado</option>
+        </select>
+        <button
+          className="btn btn-primary"
+          type="submit"
+        >
+          Agregar
+        </button>
+      </form>
+    </div>
+  );
+};
 
-
-    export default NoControlado;
+export default FormularioNoControlado;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import NoControlado from './Components/NoControlado.jsx'
+import FormularioNoControlado from './Components/NoControlado.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <NoControlado />
+    <FormularioNoControlado />
   </React.StrictMode>,
 )
